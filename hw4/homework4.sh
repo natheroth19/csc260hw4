83d07708de8eeb77bc7dbe3bda48e22e0fc96379
Nathan Avery <nathan.avery@assumption.edu>
2024-02-28 17:53:40 -0500

    Added homework4.sh and menu.txt

---
+++ hw4/homework4.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+####################################
+# Name: <Your name here>
+# CSC 260 - Spring 2024
+# Homework 4
+####################################
+
+function print_link {
+  # Type your answer to problem #1 below this line. Change XXXXXX to your student ID
+  echo "https://github.com/benztran/csc260sp24-hw4-XXXXXX"
+}
+
+function problem1 {
+  # Type your answer to problem #1 below this line
+}
+
+function problem2 {
+  # Type your answer to problem #2 below this line
+}
+
+function problem3 {
+  # Type your answer to problem #3 below this line
+}
+
+function problem4 {
+  # Type your answer to problem #4 below this line
+}
+
+function problem5 {
+  # Type your answer to problem #5 below this line
+}
+
+function problem6 {
+  # Type your answer to problem #6 below this line
+}
+
+function problem7 {
+  # Type your answer to problem #7 below this line
+}
+
+
+
